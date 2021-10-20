18518bf011ddeb2622075126cac8aa63a0840984
Akil Ahmed <akilahmed.b15@gmail.com>
2021-10-20 18:45:59 +0600

    updated 1

--- js/observers.js
+++ js/observers.js
@@ -7,3 +7,17 @@ const sliders = document.querySelectorAll(".slide-in");
 const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px"
 };
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
