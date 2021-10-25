d9562a25bc515965aa8b37ada286e1de70d992bf
Akil Ahmed <akilahmed.b15@gmail.com>
2021-10-25 20:05:26 +0600

    v0.5

--- js/observers.js
+++ js/observers.js
@@ -21,3 +21,25 @@ const sectionOneObserver = new IntersectionObserver(function(
   });
 },
 sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
